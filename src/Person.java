d08b9c0593a1792eba800cc2de8497f5a664323b
secretnastoyachiy <itsmywaytoamerica@gmail.com>
2021-12-29 18:00:10 +0600

    2

--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,10 @@ public class Person {
     private String name;
     private int age;
 
+    public Person() {
+        this.name = "default";
+    }
+
     // getter
     public String getName() {
         return name;
